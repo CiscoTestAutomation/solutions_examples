129e6b793811f2b5214afccce3174579edc69426
Siming Yuan <siyuan@cisco.com>
2019-05-16 13:17:49 -0400

    adding chaos monkey script from Kevin Corbin

---
+++ netchaos/README.md
@@ -0,0 +1,31 @@
+# netchaos
+
+Proof of concept Network Chaos Monkey using Cisco VIRL, pyATS, and Genie
+
+# Requirements
+
+pyATS requires python3 make sure you've installed the requirements in your virtualenv
+
+```
+pip install -r requirements.txt
+```
+
+# Configuration
+
+you should only need to modify the [./mapping_datafile.yaml](./mapping_datafile.yaml) to
+reflect the proper mappings to your `default_testbed.yaml` (**you must provide**) for details on creating one check [here](https://pubhub.devnetcloud.com/media/pyats/docs/topology/example.html)
+
+
+# Let the chaos begin
+
+```
+make chaos
+```
+
+# Wait not so fast....
+
+If you're scared - maybe start out with a single [Trigger](https://pubhub.devnetcloud.com/media/pyats-packages/docs/genie/genie_libs/#/triggers)
+
+```
+pyats run job network_chaos_monkey.py --testbed-file default_testbed.yaml --html-logs ./TriggerClearIpMroute.html --trigger TriggerClearIpMroute
+```
---
+++ netchaos/network_chaos_monkey.py
@@ -0,0 +1,80 @@
+import os
+from ats.datastructures.logic import And, Not, Or
+from genie.harness.main import gRun
+import argparse
+
+# chaos only knows..
+import random
+
+# how brave are you?
+# comprehensive list of triggers / docs located at
+# https://pubhub.devnetcloud.com/media/pyats-packages/docs/genie/genie_libs/#/triggers
+
+POSSIBLE_TRIGGERS = [
+    'TriggerClearBgpAll',
+    'TriggerClearIpOspfNeighborVrfAll',
+    'TriggerClearIpRoute',
+    'TriggerUnconfigConfigEvpnVni',
+    'TriggerUnconfigConfigVlanInterface',
+    'TriggerUnconfigConfigVlanVnsegment',
+    # 'TriggerClearIpMroute'
+
+]
+
+RANDOM_TRIGGER = random.choice(POSSIBLE_TRIGGERS)
+
+
+""" KNOWN_ISSUES """
+
+
+"""
+'TriggerUnconfigConfigNvOverlayEvpn'
+
+
+'TriggerUnconfigConfigBgp'
+
+2019-01-16T22:55:58: %AETEST-ERROR: Failed reason: Failed to unconfigure feature
+2019-01-16T22:55:58: %AETEST-ERROR:
+2019-01-16T22:55:58: %AETEST-ERROR: Exception:
+2019-01-16T22:55:58: %AETEST-ERROR: Traceback (most recent call last):
+2019-01-16T22:55:58: %AETEST-ERROR:   File "/Users/kecorbin/VirtualEnvs/netchaos/lib/python3.6/site-packages/genie/libs/sdk/triggers/unconfigconfig/unconfigconfig.py", line 100, in unconfigure
+2019-01-16T22:55:58: %AETEST-ERROR:     self.mapping.unconfigure(device=uut, abstract=abstract, steps=steps)
+2019-01-16T22:55:58: %AETEST-ERROR:   File "/Users/kecorbin/VirtualEnvs/netchaos/lib/python3.6/site-packages/genie/libs/sdk/libs/utils/mapping.py", line 1071, in unconfigure
+2019-01-16T22:55:58: %AETEST-ERROR:     return self.configure(unconfig=True, *args, **kwargs)
+2019-01-16T22:55:58: %AETEST-ERROR:   File "/Users/kecorbin/VirtualEnvs/netchaos/lib/python3.6/site-packages/genie/libs/sdk/libs/utils/mapping.py", line 1156, in configure
+2019-01-16T22:55:58: %AETEST-ERROR:     unconfig, name, **kwarg)
+2019-01-16T22:55:58: %AETEST-ERROR:   File "/Users/kecorbin/VirtualEnvs/netchaos/lib/python3.6/site-packages/genie/libs/sdk/libs/utils/mapping.py", line 1163, in _configure
+2019-01-16T22:55:58: %AETEST-ERROR:     co = abstracted_ops(device=device, **kwargs)
+2019-01-16T22:55:58: %AETEST-ERROR:   File "/Users/kecorbin/VirtualEnvs/netchaos/lib/python3.6/site-packages/genie/libs/conf/bgp/bgp.py", line 1288, in __init__
+2019-01-16T22:55:58: %AETEST-ERROR:     self.bgp_id = int(bgp_id)
+2019-01-16T22:55:58: %AETEST-ERROR: TypeError: int() argument must be a string, a bytes-like object or a number, not 'list'
+
+"""
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--trigger',
+                        dest='trigger',
+                        default=None)
+    # parser.add_argument('--inventory',
+    #                     dest='inventory',
+    #                     default='inventory/test.yaml')
+    args, unknown = parser.parse_known_args()
+
+    test_path = os.path.dirname(os.path.abspath(__file__))
+    # print(args)
+
+    # mapping_datafile is mandatory
+    # trigger_uids limit which test to execute
+
+    if args.trigger:
+        trigger = args.trigger
+    else:
+        trigger = RANDOM_TRIGGER
+
+
+    gRun(mapping_datafile=os.path.join(test_path, 'mapping_datafile.yaml'),
+         pts_datafile='pts_datafile.yaml',
+         pts_features=['ospf', 'bgp'],
+         trigger_uids=Or(trigger))
