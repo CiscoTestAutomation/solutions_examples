468b7868e47f9afcb54542a79ff3c4fd629de90e
simon <developer@devnetsandbox.cisco.com>
2020-02-12 20:30:38 +0000

    Each Interface individual test, looping over testcases

--- crc_errors/CRC_Count_check.py
+++ crc_errors/CRC_Count_check.py
@@ -7,8 +7,8 @@ import logging
 from tabulate import tabulate
 
 # Needed for aetest script
-from ats import aetest
-from ats.log.utils import banner
+from pyats import aetest
+from pyats.log.utils import banner
 
 # Genie Imports
 from genie.testbed import load
@@ -16,7 +16,6 @@ from genie.testbed import load
 
 # Get your logger for your script
 log = logging.getLogger(__name__)
-
 
 ###################################################################
 #                  COMMON SETUP SECTION                           #
@@ -36,105 +35,88 @@ class common_setup(aetest.CommonSetup):
                 f"Connect to device '{device.name}'"))
             try:
                 device.connect()
+                device_list.append(device)
             except Exception as e:
-                self.failed(f"Failed to establish connection to '{device.name}'")
+                log.info(f"Failed to establish connection to '{device.name}'")
 
-            device_list.append(device)
-
-        # Pass list of devices the to testcases
         self.parent.parameters.update(dev=device_list)
-        print(self.parent.parameters)
+        log.debug(self.parent.parameters)
+
+    @aetest.subsection
+    def create_testcases(self):
+        #below creates a loop over the CRC_count_Check Class using dev as the iterator
+        aetest.loop.mark(CRC_count_Check, dev=self.parent.parameters['dev'])
+
+class CRC_count_Check(aetest.Testcase):
+    @aetest.setup
+    def setup(self, dev):
+        #Setup has been marked for looping in Common Setup(create_testcases) with the argument dev
+        #dev is the list of devices in the current testbed
+        log.info(banner(f"Gathering Interface Information from {dev.name}"))
+        self.interface_info = dev.learn('interface')
 
 
-###################################################################
-#                     TESTCASES SECTION                           #
-###################################################################
-
-# Testcase name : Check for CRC errors
-class CRC_count_check(aetest.Testcase):
-    """ This is user Testcases section """
-
-    # First test section
-    @ aetest.test
-    def learn_interfaces(self):
-
-        self.all_interfaces = {}
-        for dev in self.parent.parameters['dev']:
-            log.info(banner(f"Gathering Interface Information from {dev.name}"))
-            intf = dev.learn('interface')
-            self.all_interfaces[dev.name] = intf.info
-
-    # Second test section
-    @ aetest.test
-    def check_CRC(self):
-
+        list_of_interfaces=self.interface_info.info.keys()
         mega_dict = {}
+        mega_dict[dev.name] = {}
         mega_tabular = []
-        for device, ints in self.all_interfaces.items():
-            mega_dict[device] = {}
-            for name, props in ints.items():
-                counters = props.get('counters')
-                if counters:
-                    smaller_tabular = []
-                    if 'in_crc_errors' in counters:
-                        mega_dict[device][name] = counters['in_crc_errors']
-                        smaller_tabular.append(device)
-                        smaller_tabular.append(name)
-                        smaller_tabular.append(str(counters['in_crc_errors']))
-                        if counters['in_crc_errors']:
-                            smaller_tabular.append('Failed')
-                        else:
-                            smaller_tabular.append('Passed')
+        passing=0
+
+
+        for ints, props in self.interface_info.info.items():
+            counters = props.get('counters')
+            if counters:
+                smaller_tabular = []
+                if 'in_crc_errors' in counters:
+                    mega_dict[dev.name][ints] = counters['in_crc_errors']
+                    smaller_tabular.append(dev.name)
+                    smaller_tabular.append(ints)
+                    smaller_tabular.append(str(counters['in_crc_errors']))
+                    if counters['in_crc_errors']:
+                        smaller_tabular.append('Failed')
+                        passing=1
                     else:
-                        mega_dict[device][name] = None
-                        smaller_tabular.append(device)
-                        smaller_tabular.append(name)
-                        smaller_tabular.append('N/A')
-                        smaller_tabular.append('N/A')
-                mega_tabular.append(smaller_tabular)
+                        smaller_tabular.append('Passed')
 
-        mega_tabular.append(['-'*sum(len(i) for i in smaller_tabular)])
+                else:
+                    mega_dict[dev.name][ints] = None
+                    smaller_tabular.append(dev.name)
+                    smaller_tabular.append(ints)
+                    smaller_tabular.append('N/A')
+                    smaller_tabular.append('N/A')
+            mega_tabular.append(smaller_tabular)
+        mega_tabular.append(['-' * sum(len(i) for i in smaller_tabular)])
 
-        log.info(tabulate(mega_tabular,
+        #pass megadict to interface test function
+        self.parent.parameters.update(mega=mega_dict[dev.name])
+        #pass megatable list to table_display test function
+        self.parent.parameters.update(megatable=mega_tabular)
+        #pass passing variable to table_display function in order indicate pass or fail - 0=pass 1=fail
+        self.parent.parameters.update(passing=passing)
+
+        aetest.loop.mark(self.interface_check, intf=list_of_interfaces)
+
+    @aetest.test
+    def table_display(self):
+        log.info(tabulate(self.parent.parameters['megatable'],
                           headers=['Device', 'Interface',
                                    'CRC Errors Counter',
                                    'Passed/Failed'],
                           tablefmt='orgtbl'))
 
-        for dev in mega_dict:
-            for intf in mega_dict[dev]:
-                if mega_dict[dev][intf]:
-                    self.failed(f"{dev}: {intf} CRC ERRORS: {mega_dict[dev][intf]}")
+        if self.parent.parameters['passing']==1:
+            self.failed('Some interfaces have CRC errors')
+        else:
+            self.passed
 
-        self.passed("All devices' interfaces CRC ERRORS Count is: 'Zero'")
-
-# #####################################################################
-# ####                       COMMON CLEANUP SECTION                 ###
-# #####################################################################
-
-
-# This is how to create a CommonCleanup
-# You can have 0 , or 1 CommonCleanup.
-# CommonCleanup can be named whatever you want :)
-class common_cleanup(aetest.CommonCleanup):
-    """ Common Cleanup for Sample Test """
-
-    # CommonCleanup follow exactly the same rule as CommonSetup regarding
-    # subsection
-    # You can have 1 to as many subsections as wanted
-    # here is an example of 1 subsection
-
-    # @aetest.subsection
-    # def clean_everything(self):
-    #     """ Common Cleanup Subsection """
-    #     log.info("Aetest Common Cleanup ")
-    @aetest.subsection
-
-    def disconnect(self):
-        log.info("Aetest Common Cleanup disconnecting devices")
-        for dev in self.parent.parameters['dev']:
-            dev.disconnect()
+    @aetest.test
+    def interface_check(self, intf):
+        # This test has been marked for loop.  intf is the looping argument (list of interfaces)
+        # Thus this test is run for each interface in the intf list.
+        for int, errors in self.parent.parameters['mega'].items():
+            if errors:
+                self.failed(f'Interface {int} has crc errors {errors}')
+            else:
+                self.passed(f'No errors on {int}')
 
 
-if __name__ == '__main__':  # pragma: no cover
-    aetest.main()
