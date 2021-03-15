04909f19a03b46a6091b0c291dc5734fc8220506
Matt Siegel <msiegy88@gmail.com>
2021-03-14 20:06:47 -0400

    Update ping_test_job.py
    Add argparse for custom list of IP addresses

--- ping/ping_test_job.py
+++ ping/ping_test_job.py
@@ -8,18 +8,32 @@ __credits__ = ["hapresto@cisco.com"]
 __version__ = 1.0
 
 import os
+import argparse
 from pyats.easypy import run
 
+# command line argument parser
+# see https://pubhub.devnetcloud.com/media/pyats/docs/easypy/jobfile.html#custom-arguments
+parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dest",
+        dest = "ping_list",
+        type=str,
+        default = "208.67.222.222 8.8.8.8 1.1.1.1",
+        help = "space delimted list of IP address(es) to test connectivity"
+    )
 # compute the script path from this location
 SCRIPT_PATH = os.path.dirname(__file__)
 
-
 def main(runtime):
     """job file entrypoint"""
+    # parse command line arguments
+    # only parse arguments we know
+    args, _ = parser.parse_known_args()
 
-    # run script
+    # run script, pass arguments to script as parameters
     run(
         testscript=os.path.join(SCRIPT_PATH, "ping_test.py"),
         runtime=runtime,
         taskid="Ping",
+        **vars(args)
     )
